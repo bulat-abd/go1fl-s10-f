1e4b7eeae428093f218ec55002865f132f23350c
Bulat <babdullatypov@gmail.com>
2025-10-18 21:51:40 +0300

    Copy contents of dir2 into dir3/dir4

--- task.sh
+++ task.sh
@@ -12,7 +12,7 @@ chmod 764 ./dir2/hello.sh
 
 ls -1 ./dir2 > ./dir2/list.txt
 
-# копируем содержимое каталога task/dir2 в каталог task/dir3/dir4
+cp -R ./dir2/ ./dir3/dir4
 
 # записываем в task/dir1/summary.txt список файлов с расширением *.txt
 # находящихся в task, включая поддиректории
